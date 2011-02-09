aae91ed08b5d281dce80abc3f80fda672bb8b031
s.bando <the.skeleton7@gmail.com>
2011-02-09 11:40:05 +0900

    プロジェクト(の枠)を新規作成

---
+++ src/jp/ranmelo/Ranmelo.java
@@ -0,0 +1,13 @@
+package jp.ranmelo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Ranmelo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
